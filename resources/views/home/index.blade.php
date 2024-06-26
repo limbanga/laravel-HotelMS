7a29a964322f7b9a4250d97e8efc8a55261ecefe
limbanga <95570833+limbanga@users.noreply.github.com>
2024-06-26 17:21:12 +0700

    init project :3

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Trang Chủ Đầu Tiên</title>
+</head>
+<body>
+    <h1>Chào mừng bạn đến với trang chủ đầu tiên của Laravel!</h1>
+    <p>Đây là trang chủ đầu tiên bạn tạo trong Laravel.</p>
+</body>
+</html>
